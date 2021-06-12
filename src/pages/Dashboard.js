56dd0221bfa7efed821ae633f37f9085bb7c97b2
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2021-06-12 19:52:50 +0530

    Logout Functionality added

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,5 +1,62 @@
+import { useState } from "react";
+import {
+  IconButton,
+  InputAdornment,
+  makeStyles,
+  Snackbar,
+  OutlinedInput,
+  TextField,
+  Card,
+  CardContent,
+  Button,
+  CardHeader,
+  CardMedia,
+} from "@material-ui/core";
+import {
+  Email,
+  ExitToApp,
+  LockSharp,
+  Visibility,
+  VisibilityOff,
+} from "@material-ui/icons";
+import { useAppContext } from "../hooks";
+import { Alert } from "@material-ui/lab";
+const useStyles = makeStyles(() => ({
+  pageWrapper: {
+    display: "flex",
+    height: "100vh",
+    width: "100vw",
+    alignItems: "center",
+    justifyContent: "center",
+    background: "linear-gradient(326deg, #a4508b 0%, #5f0a87 74%)",
+  },
+  cardWrapper: {
+    display: "flex",
+  },
+}));
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const classes = useStyles();
+  const { setIsLoggedIn } = useAppContext();
+
+  return (
+    <>
+      <div className={classes.pageWrapper}>
+        <Card>
+          <CardHeader
+            title="Dashboard"
+            action={
+              <IconButton
+                aria-label="logout"
+                onClick={() => setIsLoggedIn(false)}
+              >
+                <ExitToApp />
+              </IconButton>
+            }
+          />
+        </Card>
+      </div>
+    </>
+  );
 };
 
 export default Dashboard;

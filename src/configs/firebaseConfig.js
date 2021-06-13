b1ce23cb0343925bd45cbf985ce3fddee7984b6b
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2021-06-13 09:44:09 +0530

    Firebase installed

---
+++ src/configs/firebaseConfig.js
@@ -0,0 +1,50 @@
+import firebase from "firebase";
+// Import Firebase Auth Module
+import "firebase/auth";
+// Import database from the Firebase
+import "firebase/database";
+// Import storage from the Firebase
+import "firebase/storage";
+// Import analytics from the Firebase
+import "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBM12QNpOOpTK7UVvqovNGhHDYyIQE5Vvk",
+  authDomain: "firecrud-aa9ec.firebaseapp.com",
+  databaseURL: "https://firecrud-aa9ec.firebaseio.com",
+  projectId: "firecrud-aa9ec",
+  storageBucket: "firecrud-aa9ec.appspot.com",
+  messagingSenderId: "513744767392",
+  appId: "1:513744767392:web:119d789ef9679bdf6cbcc5",
+  measurementId: "G-KZSM3EL0N6",
+};
+
+// Configure FirebaseUI.
+export const uiConfig = {
+  // Popup signin flow rather than redirect flow.
+  signInFlow: "popup",
+  // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
+  signInSuccessUrl: "/",
+  // We will display Google and Facebook as auth providers.
+  signInOptions: [
+    {
+      provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    },
+    {
+      provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    },
+  ],
+};
+
+// NOTE Initialize the app
+const app = firebase.initializeApp(firebaseConfig);
+
+export const auth = app.auth();
+
+export const database = app.database();
+
+export const storage = app.storage();
+
+export const analytics = app.analytics();
+
+export default app;
---
+++ src/configs/index.js
@@ -0,0 +1,2 @@
+import { auth, uiConfig, database, storage } from "./firebaseConfig";
+export { auth, uiConfig, database, storage };
